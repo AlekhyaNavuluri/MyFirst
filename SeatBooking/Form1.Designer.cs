d493c9e70108a59f455cc13d462397579fbd64a3
Alekhya <anilalekhya@gmail.com>
2015-06-24 22:47:14 +0100

    seat booking

---
+++ SeatBooking/Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+namespace SeatBooking
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            int left = 100;
+            int top = 100;
+            for (int j = 1; j <= 10; j++)
+            {
+                for (int i = 1; i <= 12; i++)
+                {
+                    var button = new Button();
+                    button.Name = i.ToString();
+                    button.Text = i.ToString();
+                    button.Left = left;
+                    button.Top = top;
+                    left += 90;
+                    this.Controls.Add(button);
+                    button.Click += new EventHandler(this.Btn_Click);
+                }
+
+                left = 100;
+                top += 40;
+            }
+
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.WindowState = FormWindowState.Maximized;
+
+        }
+
+        #endregion
+
+        
+
+    }
+}
+
---
+++ SeatBooking/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SeatBooking
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void Btn_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender ;
+            if (button.Text == "B")
+            {
+                button.Text = button.Name;
+                button.BackColor = Color.White;
+            }
+            else
+            {
+                button.Text = "B";
+                button.BackColor = Color.Green;
+            }
+
+            using (StreamWriter w = File.AppendText(@"C:\Temp\log.txt"))
+            {
+                w.WriteLine(button.Text);
+                w.WriteLine(button.Name);
+            }
+        }
+    }
+}
+
+
